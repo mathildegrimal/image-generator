b5ad3e534fc0967af594561a3ad72579ff866bce
Mathilde <mat.Grimal@gmail.com>
2022-02-23 13:11:26 +0100

    rebased

--- client/src/HomePage/HomePage.tsx
+++ client/src/HomePage/HomePage.tsx
@@ -1,39 +1,35 @@
 import React, { useState } from "react";
-import axios from 'axios';
-
+import FileInput from "./FileInput";
+import axios from "axios";
 export default function HomePage() {
-
-  const [image, setImage]= useState({});
-
-  const getFileInfo = (e:any) => {
-      const formData = new FormData();
-
-      //FILE INFO NAME WILL BE "my-image-file"
-      formData.append("my-image-file", e.target.files[0], e.target.files[0].name);
-      setImage(formData);
+  
+  const [images, setImages] = useState<FormData[]>([]);
+  const handleChange = (e: any) => {
+    const imagesToUpload = images;
+    const formData = new FormData();
+    images.push(formData);
+    formData.append("my-image-file", e.target.files[0], e.target.files[0].name);
+    setImages(imagesToUpload);
+    console.log(images);
   }
-
   const handleClick = () => {
-    console.log(image);
-    axios.post('http://localhost:8000/image-upload', 
-      image
-    )
-    .then((res : any) => {
-        console.log('Axios response: ', res)
-    })
-  }
+    for (const image of images) {
+      axios.post("http://localhost:8000/image-upload", image);
+    }
+  };
 
   return (
     <div className="h-screen w-screen flex justify-center items-center">
       <div className="w-auto p-4 h-auto bg-cyan-100 rounded-xl shadow-xl">
         <div className="flex  items-center justify-center bg-grey-lighter">
           <div className="flex-col items-center">
-            <input type="file" onChange={getFileInfo}/>
-            <input type="file" onChange={getFileInfo}/>
+            <input type="file" onChange={handleChange} />
+            <input type="file" onChange={handleChange} />
           </div>
           <button
-            onClick={handleClick}
+            type="submit"
             className="border-2 p-2 ml-2 rounded-xl bg-slate-100 shadow-lg"
+            onClick={handleClick}
           >
             Envoyer
           </button>
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+const multer = require("multer");
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
